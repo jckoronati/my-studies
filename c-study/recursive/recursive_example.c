7691469079e0ed100457c8f48f61b8af4b705c44
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-21 12:23:43 -0300

    feat: implemented recursive example

---
+++ c-study/recursive/recursive_example.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void prints(int number) 
+{
+    if (number == 0)
+        printf("\n%d\n", number);
+    else 
+    {
+        printf("%d ", number);
+        prints(number - 1);
+        printf("%d ", number);
+        printf("\n");
+    }    
+}
+
+int main()
+{
+    int number;
+    printf("Enter a number greater than 0: ");
+    scanf("%d",&number);
+    prints(number);
+    return 0;
+}
