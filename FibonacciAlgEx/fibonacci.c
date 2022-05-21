9bbc65e36c697d1358ff4db4facde52b9e3887f8
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-21 11:27:37 -0300

    feat: implemented fibonacci sequence calculator

---
+++ FibonacciAlgEx/fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int count = 1, firstValue = 0, secondValue = 1, auxiliary, value;
+
+    scanf("%d", &value);
+
+    while (count <= value)
+    {
+        printf("%d ", firstValue);
+        auxiliary = firstValue + secondValue;
+        firstValue = secondValue;
+        secondValue = auxiliary;
+        count++;
+    }
+
+    return 0;
+}
