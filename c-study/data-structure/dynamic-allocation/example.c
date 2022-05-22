283307f6dba3eb3151dd17740000560c39517e73
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-22 05:32:13 -0300

    feat: implemented an example of dynamic allocation

---
+++ c-study/data-structure/dynamic-allocation/example.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// defines an struct
+struct point {
+    float x;
+    float y;
+};
+
+// implemented def for simplife use
+typedef struct point Point;
+
+int main(void)
+{
+    // creates a variable type Point to allocate pointer to x and y
+    Point *p = (Point*) malloc(sizeof(Point));
+    p->x = 1;
+    p->y = 5;
+
+    printf("\nO valor do ponto x é: %.2f", p->x);
+    printf("\nO valor do ponto y é: %.2f", p->y);
+    printf("\n");
+
+    return 0;
+}
