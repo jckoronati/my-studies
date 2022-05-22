9e9291b3e778381990b71b434eed316a38682126
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-05-22 06:06:49 -0300

    feat: implemented list

--- c-study/data-structure/lists/example.c
+++ c-study/data-structure/lists/example.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void createPoint(float x, float y);
 
 struct point
 {
     float x;
     float y;
+    struct point *next;
+};
+
+typedef struct point Point;
+
+Point *list;
+
+void createPoint(float x, float y)
+{
+    Point *p = (Point*) malloc(sizeof(Point));
+    p->x = x;
+    p->y = y;
+    p->next = list;
+    list = p;
 }
 
 int main(void)
 {
+    createPoint(1, 5);
+    printf("%.2f \n", list->x);
+    printf("\n");
     return 0;
 }
