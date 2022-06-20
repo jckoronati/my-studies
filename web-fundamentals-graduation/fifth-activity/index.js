d62d63f080966c8970cc485e4425b0c2c903bd7d
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-06-20 20:19:45 -0300

    feat: implemented last fifth activity

--- web-fundamentals-graduation/fifth-activity/index.js
+++ web-fundamentals-graduation/fifth-activity/index.js
@@ -1,9 +1,33 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+
+const books = [
+    {
+        id: 1,
+        name: "JavaScript do Básico ao Avançado",
+        price: 52.75
+    },
+    {
+        id: 2,
+        name: "Rust in Action",
+        price: 72.90
+    }
+];
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('home', {books});
+});
+
+app.get('/book/:id', (req, res) => {
+    const book = books.find((item) => parseInt(item.id) === parseInt(req.params.id));
+    
+    res.render('book', {book});
 });
 
 app.listen(3000, () => console.log("The server is running on 3000 port"));
